2425f5aec38617178913d95ba9d8c621b289a6a5
rahul yadav <rahul.yadav@altudo.co>
2022-11-23 23:44:40 +0530

    a

--- config/database.js
+++ config/database.js
@@ -5,6 +5,12 @@ module.exports = ({ env }) => ({
     client: 'sqlite',
     connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
+      options: {
+        ssl: env.bool('DATABASE_SSL', false),
+      },
     },
     useNullAsDefault: true,
   },
